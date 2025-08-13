d08ce47ff556989d87a0d4816d72c70ad5dd0ffb
mythripanugatlagoud <mp9816@srmist.edu.in>
2025-08-14 00:04:29 +0530

    Added Step 1 solutions

---
+++ Step1_Basics/01_InputOutput.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    cout << x;
+    return 0;
+}
+// class Solution {
+// public:
+//     void printNumber() {
+//         int num;
+//         cin >> num;        // take input
+//         cout << num;       // print output
+//     }
+// };
+
+
+
